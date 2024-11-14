b4f4a0392a43fe98c1ab04145aa93320d6af66c3
roy <roysch33@gmail.com>
2024-11-14 13:54:31 +0200

    finish TestRandom

--- TestRandom.java
+++ TestRandom.java
@@ -2,5 +2,20 @@
 public  class  TestRandom {
 	public static void main(String[]  args) {
 	    // Replace this comment with your code
+		int timesRandom = Integer.parseInt(args[0]);
+		int lessThenHalf = 0;
+		int moreThenHalf = 0;
+
+		for (int i = 0; i < timesRandom; i++ ){
+			double randomNumber = (Math.random() );
+			if (randomNumber > 0.5){
+				moreThenHalf++; 
+			}
+			else {
+				lessThenHalf++;
+			}
+		}
+		System.out.println("> 0.5: " + moreThenHalf + " times" );
+		System.out.println("<= 0.5: " + lessThenHalf + " times" );
 	}
 }
